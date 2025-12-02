717f9ea1e4d3f1bdc1fdbd8dbfe3b69e4baf5dae
Flávio Henrique <flaviopradela26@gmail.com>
2025-12-02 19:08:18 -0300

    create 7

--- JS/exercicio7/script.js
+++ JS/exercicio7/script.js
@@ -1,5 +1,8 @@
 const principal = document.getElementById("principal");
-const imagens = document.getElementById("imagens");
+const miniaturas = document.querySelectorAll("#imagens img");
 
-
-
+miniaturas.forEach(function(img) {
+    img.addEventListener("click", function() {
+        principal.src = img.src;
+    });
+});
