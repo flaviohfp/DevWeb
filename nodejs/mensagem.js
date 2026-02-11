1177dadb9f56daf840ef56085df96e088960ada3
Flávio Henrique <flaviopradela26@gmail.com>
2026-02-11 15:56:57 -0300

    create exercise

--- nodejs/mensagem.js
+++ nodejs/mensagem.js
@@ -1,3 +1,8 @@
-let nome = process.argv[2];
+let num1 = 6;
+let num2 = 9;
 
-console.log("bme vindo, ", nome);
+let soma = num1 + num2;
+let multiplicacao = num1 * num2;
+
+console.log("soma: ", soma);
+console.log("multiplicacao: ", multiplicacao);
