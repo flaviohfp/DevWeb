8a1191f17434649c719cdb0f20f65d853049b39a
Flávio Henrique <flaviopradela26@gmail.com>
2026-02-11 15:49:21 -0300

    create exercise 4

--- nodejs/mensagem.js
+++ nodejs/mensagem.js
@@ -1,12 +1,3 @@
-let nome = "Jose dos porco";
-let classe = "mago dos porco";
-let nivel = 20;
-let vida = 1000;
-let mana = 1000;
+let nome = process.argv[2];
 
-
-console.log("nome: ", nome);
-console.log("classe", classe);
-console.log("nivel: ", nivel);
-console.log("vida: ", vida);
-console.log("mana: ", mana);
+console.log("bme vindo, ", nome);
