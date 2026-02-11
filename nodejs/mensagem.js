b5d5d8165e3cd82f2865711508a2feccd22a7b26
Flávio Henrique <flaviopradela26@gmail.com>
2026-02-11 16:13:38 -0300

    create exercise 8

--- nodejs/mensagem.js
+++ nodejs/mensagem.js
@@ -1,16 +1,4 @@
 let nivel = process.argv[2];
+let xp = nivel * 100;
 
-
-console.log("===== Nivel do personagem =====");
-
-
-console.log("Nivel:", nivel);
-
-if (nivel >= 1 || nivel <= 10) {
-    console.log("Iniciante");
-} else if (nivel >= 10 || nivel <= 30) {
-    console.log("Intermediario");
-} else {
-    console.log("Veterano");
-}
-    
+console.log("xp necessario para o proximo nivel: ", xp);
