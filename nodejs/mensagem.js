140ec74833be422d5b3560cb49010b3048cf7078
Flávio Henrique <flaviopradela26@gmail.com>
2026-02-11 16:00:07 -0300

    create exercise 6

--- nodejs/mensagem.js
+++ nodejs/mensagem.js
@@ -1,8 +1,13 @@
-let num1 = 6;
-let num2 = 9;
 
-let soma = num1 + num2;
-let multiplicacao = num1 * num2;
+let idade = process.argv[2];
 
-console.log("soma: ", soma);
-console.log("multiplicacao: ", multiplicacao);
+
+
+
+console.log("Idade:", idade);
+
+if (idade >= 18) {
+    console.log("maior de idade");
+} else {
+    console.log("menor de idade");
+}
