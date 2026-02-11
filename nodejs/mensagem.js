ee01839b4ba61ecfde5f103c6314979d23b2eb7f
Flávio Henrique <flaviopradela26@gmail.com>
2026-02-11 15:33:13 -0300

    create exercise 2

--- nodejs/mensagem.js
+++ nodejs/mensagem.js
@@ -1,5 +1,7 @@
-console.log("Flavio Henrique ");
+let numero1 = 545;
+let numero2 = 63;
 
-console.log("ADS");
-
-console.log("Para Bens");
+console.log("soma: ", numero1 + numero2);
+console.log("subtracao: ", numero1 - numero2);
+console.log("divisao: ", numero1 / numero2);
+console.log("multiplicacao: ", numero1 * numero2);
