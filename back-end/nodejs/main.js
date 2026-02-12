0c2e72e9aeddbb8efb5cc214235c75c8b987f15b
Flávio Henrique <flaviopradela26@gmail.com>
2026-02-12 14:43:22 -0300

    create mod2 exe3

--- back-end/nodejs/main.js
+++ back-end/nodejs/main.js
@@ -1,9 +1,13 @@
-const mensagens = require("./mensagens");
+const {validarNome, validarIdade} = require("./validacoes");
 
-const nomeUsuario = "Flavio";
+console.log("-----Teste de nome----");
 
-console.log(mensagens.boasVindas(nomeUsuario));
+console.log(validarNome("Flavio"));
+console.log(validarNome("Fl"));
+console.log(validarNome(12));
 
-console.log(mensagens.despedida(nomeUsuario));
+console.log("----Teste de idade----");
 
-console.log("Sistema criado por", mensagens.autorSistema);
+console.log(validarIdade(20));
+console.log(validarIdade(-34));
+console.log(validarIdade("12"));
