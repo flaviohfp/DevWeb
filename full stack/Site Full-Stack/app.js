1a67bd49fcc1fc13af981777413c50c792f0c397
Flávio Henrique <flaviopradela26@gmail.com>
2026-03-18 15:00:25 -0300

    update

---
+++ full stack/Site Full-Stack/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const usuariosRoutes = require("./routes/usuarios");
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use("/api/usuarios", usuariosRoutes);
+
+app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
