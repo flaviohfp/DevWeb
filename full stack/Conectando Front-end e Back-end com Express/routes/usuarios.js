ae4eae0c97748cd223083f77a5f27921e7e65ec1
Flávio Henrique <flaviopradela26@gmail.com>
2026-03-18 14:51:05 -0300

    update project

--- full stack/Conectando Front-end e Back-end com Express/routes/usuarios.js
+++ full stack/Conectando Front-end e Back-end com Express/routes/usuarios.js
@@ -1,77 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-let usuarios = [];
-let proximoId = 1;
+const usuariosController = require("../controllers/usuariosController");
 
+router.get("/", usuariosController.listarUsuarios);
 
-router.get("/api/usuarios", (req, res) => {
-  res.status(200).json(usuarios);
-});
+router.get("/:id", usuariosController.buscarUsuario);
 
-router.get('/api/usuarios/:id', (req, res) => {
+router.post("/", usuariosController.criarUsuario);
 
-    const id = Number(req.params.id);
+router.put("/:id", usuariosController.atualizarUsuario);
 
-    const usuario = usuarios.find(u => u.id === id);
-
-    if (!usuario) {
-        return res.status(404).json({ erro: "Usuário não encontrado" });
-    }
-
-    res.json(usuario);
-});
-
-router.post("/api/usuarios", (req, res) => {
-  const { nome, idade } = req.body;
-
-  if (!nome || nome.trim() === "") {
-    return res.status(400).json({
-      erro: "Nome é obrigatório",
-    }); 
-  }
-
-  const novoUsuario = {
-    id: proximoId++,
-    nome,
-    idade,
-  };
-
-  usuarios.push(novoUsuario);
-
-  res.status(201).json(novoUsuario);
-});
-
-router.put('/api/usuarios/:id', (req, res) => {
-
-    const id = Number(req.params.id);
-    const { nome, idade } = req.body;
-
-    const usuario = usuarios.find(u => u.id === id);
-
-    if (!usuario) {
-        return res.status(404).json({ erro: "Usuário não encontrado" });
-    }
-    
-    usuario.nome = nome ?? usuario.nome;
-    usuario.idade = idade ?? usuario.idade;
-
-    res.json(usuario);
-});
-
-router.delete('/api/usuarios/:id', (req, res) => {
-
-    const id = Number(req.params.id);
-
-    const index = usuarios.findIndex(u => u.id === id);
-
-    if (index === -1) {
-        return res.status(404).json({ erro: "Usuário não encontrado" });
-    }
-
-    usuarios.splice(index, 1);
-
-    res.status(204).send();
-});
+router.delete("/:id", usuariosController.deletarUsuario);
 
 module.exports = router;
